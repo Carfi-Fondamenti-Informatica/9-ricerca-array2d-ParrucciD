1a6253d0217f636c6db63cfb4c4deb1c87120f05
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-12-10 11:09:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int funzione(char nomi[10][20],char nome[]);
+int main() {
+    char nomi[10][20]={0};
+    char nome[20]={0};
 
-int main(){
-  return 0;
+    for(size_t i=0;i<10;i++) cin >> nomi[i];
+
+    for(int j=0;j<20;j++) nome[j];
+    cin >> nome;
+
+    int posizione = funzione(nomi,nome);
+    if(posizione>=0){
+        cout<< posizione << endl;
+    }else{
+        cout <<"non presente"<< endl;
+    }
 }
+
