ef86a33e3ce339a5d0a50ef51e1f1c1a2b41e7af
ParrucciD <91885547+ParrucciD@users.noreply.github.com>
2021-12-10 11:08:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#include "lib.h"
+int funzione(char nomi[10][20],char nome[20]){
+for(int i=0;i<10;i++){
+bool flag=true;
+for (int j=0;j<20;j++){
+if(nomi[i][j]!=nome[j]) flag=false;
+}
+if(flag) return i;
+}
+return -1;
 
+}
